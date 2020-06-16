e754b0fe5a9d17a3b1c4092bcd9429251098621f
Adrian ;) <adrianmircor@gmail.com>
2020-06-16 02:53:49 -0500

    Funcionable. Mejorar responsive

--- src/components/layout/ModalDouble.js
+++ src/components/layout/ModalDouble.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 
+//REDUX
+import { useSelector } from "react-redux";
+
 const ModaleDouble = (props) => {
-  const { show, onHide, boolperder, pokeder, pokeizq } = props;
+  const { show, onHide, pokeder, pokeizq } = props;
+  const puntos = useSelector((state) => state.points.puntos);
 
   const fueCerrado = () => {
     console.log("Fue cerrado");
@@ -20,8 +24,30 @@ const ModaleDouble = (props) => {
     >
       <Modal.Header closeButton>
         {/*AGREGAR PERDISTE */}
-        <img className="imgModalIzq" src={pokeizq.imagen}></img>
-        <img className="imgModalDer" src={pokeder.imagen}></img>
+        <div className="container pr-0 ml-2">
+          <div className="row pt-3 d-flex justify-content-center">
+            <i className="far fa-frown-open fa-10x"></i>
+          </div>
+          <div className="row pt-3 d-flex justify-content-center">
+            <h3 className="msje">
+              ¡ PERDISTE, <span>obtuviste {puntos} pokepuntos </span>!
+            </h3>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <p className="msje pt-4">
+                El peso de {pokeizq.nombre} es {pokeizq.peso}
+              </p>{" "}
+              <img className="imgModalIzq" src={pokeizq.imagen}></img>
+            </div>
+            <div className="col-6">
+              <p className="msje pt-4">
+                El peso de {pokeder.nombre} es {pokeder.peso}
+              </p>{" "}
+              <img className="imgModalDer" src={pokeder.imagen}></img>
+            </div>
+          </div>
+        </div>
       </Modal.Header>
     </Modal>
   );
--- src/components/layout/ModalSingle.js
+++ src/components/layout/ModalSingle.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
 
+//REDUX
+import { useSelector } from "react-redux";
+
 const Modale = (props) => {
-  const { show, onHide, pokmodal, boolperder} = props;
+  const { show, onHide, pokmodal } = props;
+  const puntos = useSelector((state) => state.points.puntos);
 
   const fueCerrado = () => {
     console.log("Fue cerrado");
@@ -20,8 +24,20 @@ const Modale = (props) => {
     >
       <Modal.Header closeButton>
         {/*AGREGAR VAS BIEN */}
-        <i className="far fa-smile-beam fa-10x"></i>
-        <img className="imgModal" src={pokmodal.imagen}></img>
+        <div className="container pr-0 ml-2">
+          <div className="row pt-3 d-flex justify-content-center">
+            <i className="far fa-smile-beam fa-10x"></i>
+          </div>
+          <div className="row pt-3 d-flex justify-content-center">
+            <h3 className="msje">
+              ¡ VAS BIEN, <span>obtienes {puntos} pokepuntos </span>!
+            </h3>
+          </div>
+          <p className="msje pt-4">
+            El peso de {pokmodal.nombre} es {pokmodal.peso}
+          </p>
+          <img className="imgModal" src={pokmodal.imagen}></img>
+        </div>
       </Modal.Header>
     </Modal>
   );
